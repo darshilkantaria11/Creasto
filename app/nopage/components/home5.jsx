f5f8e66c25806fccad1cc832026b9a13480bfe38
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-03-18 18:08:31 +0530

    home page completed

---
+++ app/nopage/components/home5.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+const DescriptionBox = () => {
+    return (
+      <div className="bg-gray-100 p-4 md:p-6 lg:p-8 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Why Creasto?</h2>
+        <p className="text-lg text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat consequat libero nec sagittis. 
+          Nullam dignissim felis ac felis iaculis eleifend. Duis in justo id odio scelerisque sodales. 
+          Suspendisse fringilla arcu quis enim congue, a pulvinar velit rutrum. Mauris eget venenatis mi. 
+          Donec vel eros ac nunc fermentum consequat vitae quis orci. 
+        </p>
+      </div>
+    );
+  };
+  
+  export default DescriptionBox;
--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@
 import Home1 from "./nopage/components/home1"
 import Home2 from "./nopage/components/home2"
 import Home3 from "./nopage/components/home3"
+import Home4 from "./nopage/components/home4"
+import Home5 from "./nopage/components/home5"
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
         <Home1/>
         <Home2/>
         <Home3/>
+        <Home4/>
+        <Home5/>
     </>
   );
 }
